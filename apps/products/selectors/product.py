c3e5e3815ddacbb5dbae7fd7484e8e36d4ea08a7
suleiman <suleiman.parizadi30@gmail.com>
2026-07-04 13:24:11 +0330

    fix the filter in product selector

--- apps/products/selectors/product.py
+++ apps/products/selectors/product.py
@@ -1,6 +1,6 @@
 from apps.products.models.products import Product
 from django.shortcuts import get_object_or_404
-from django.db.models import QuerySet, Q
+from django.db.models import QuerySet
 
 
 class ProductSelector:
@@ -8,7 +8,7 @@ class ProductSelector:
         Reusable product queries
     """
     @staticmethod
-    def get_product_by_id(product_id) -> Product:
+    def get_product_by_id(product_id: int) -> Product:
 
         return get_object_or_404(
             Product.objects.select_related('category','cpu').prefetch_related('images'),
@@ -21,12 +21,12 @@ class ProductSelector:
 
         return Product.objects.filter(
                 is_active=True
-                ).select_related('category','cpu').order_by('-created_at')
+                ).select_related('category','cpu').prefetch_related('images').order_by('-created_at')
             
         
 
     @staticmethod
-    def get_product_by_slug(product_slug) -> Product:
+    def get_product_by_slug(product_slug: str) -> Product:
 
         return get_object_or_404(
             Product.objects.select_related('category', 'cpu').prefetch_related('images'),
@@ -34,23 +34,61 @@ class ProductSelector:
             slug=product_slug
         )
     
-
-    @staticmethod
-    def get_products_by_brand(product_brand) -> QuerySet:
-
-        return Product.objects.filter(
-                brand = product_brand,
-                is_active = True
-            ).select_related('category', 'cpu').order_by('-created_at')
-    
             
     
-
-    @staticmethod 
-    def get_products_by_category(category_slug) -> QuerySet:
-
-        return Product.objects.filter(
-                category__slug = category_slug,
-                is_active = True,  
-            ).select_related('category', 'cpu')
+    @staticmethod
+    def filter_product(
+            brand=None, category_slug=None, ram=None, storage=None, gpu=None, 
+            cpu_manufacturer=None, min_price=None, max_price=None,
+            in_stock_only=None, touch_screen = None,
+            min_display_size=None, max_display_size=None) -> QuerySet:
         
+
+        product = Product.objects.filter(is_active=True)
+
+        if brand:
+            product = product.filter(brand__iexact=brand)
+
+
+        if category_slug:
+            product = product.filter(category__slug=category_slug)
+
+        if ram:
+            product = product.filter(ram=ram)
+
+        if storage:
+            product = product.filter(storage=storage)
+
+        if gpu:
+            product = product.filter(gpu=gpu)
+
+        if cpu_manufacturer:
+            product = product.filter(cpu__manufacturer=cpu_manufacturer)
+
+        
+        if min_price is not None:
+            product = product.filter(price__gte=min_price)
+
+        if max_price is not None: # <--- then python treat 0 as a real value too, not a False
+            product = product.filter(price__lte=max_price)
+
+        if in_stock_only:
+            product = product.filter(stock__gt=0)
+
+        
+        if min_display_size:
+            product = product.filter(display_size__gte=min_display_size)
+
+        if max_display_size:
+            product = product.filter(display_size__lte=max_display_size)
+
+        if touch_screen is not None:
+            product = product.filter(touch_screen=touch_screen)
+
+        return product.select_related('cpu', 'category').prefetch_related('images')
+    
+
+
+
+
+
--- config/settings.py
+++ config/settings.py
@@ -3,12 +3,13 @@ from datetime import timedelta
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.evn')
 
 
 # Quick-start development settings - unsuitable for production
@@ -97,8 +98,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
