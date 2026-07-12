7f82949b7ac2eaec268dff0a58d72de8eafb2556
Suleiman Parizadi <suleiman.parizadi.30@gmail.com>
2026-07-12 14:03:32 +0330

    views are done, start testing

--- apps/cart/selectors/cart_selectors.py
+++ apps/cart/selectors/cart_selectors.py
@@ -1,11 +1,12 @@
 from apps.cart.models.cart import Cart
 
 
-
-
 class CartSelector:
+    
+    @staticmethod
+    def get_cart_by_session(session_key):
+        return Cart.objects.prefetch_related('items__product').filter(session_key=session_key).first()
 
-    def get_cart_with_items(self, cart):
-
-        return Cart.objects.filter(id=cart.id).first()
-
+    @staticmethod
+    def get_cart_by_user(user):
+        return Cart.objects.prefetch_related('items__product').filter(user=user).first()
